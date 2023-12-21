cc8ff08c51880659732882e7468ae66140115c34
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-12-21 15:06:22 +1100

    feat(ui): use dropzone over mantine for upload

--- invokeai/frontend/web/src/features/workflowLibrary/components/WorkflowLibraryMenu/UploadWorkflowMenuItem.tsx
+++ invokeai/frontend/web/src/features/workflowLibrary/components/WorkflowLibraryMenu/UploadWorkflowMenuItem.tsx
@@ -1,7 +1,7 @@
-import { FileButton } from '@mantine/core';
 import { InvMenuItem } from 'common/components';
 import { useLoadWorkflowFromFile } from 'features/workflowLibrary/hooks/useLoadWorkflowFromFile';
-import { memo, useRef } from 'react';
+import { memo, useCallback, useRef } from 'react';
+import { useDropzone } from 'react-dropzone';
 import { useTranslation } from 'react-i18next';
 import { FaUpload } from 'react-icons/fa';
 
@@ -9,18 +9,28 @@ const UploadWorkflowMenuItem = () => {
   const { t } = useTranslation();
   const resetRef = useRef<() => void>(null);
   const loadWorkflowFromFile = useLoadWorkflowFromFile({ resetRef });
+
+  const onDropAccepted = useCallback(
+    (files: File[]) => {
+      if (!files[0]) {
+        return;
+      }
+      loadWorkflowFromFile(files[0]);
+    },
+    [loadWorkflowFromFile]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { 'application/json': ['.json'] },
+    onDropAccepted,
+    noDrag: true,
+    multiple: false,
+  });
   return (
-    <FileButton
-      resetRef={resetRef}
-      accept="application/json"
-      onChange={loadWorkflowFromFile}
-    >
-      {(props) => (
-        <InvMenuItem as="button" icon={<FaUpload />} {...props}>
-          {t('workflows.uploadWorkflow')}
-        </InvMenuItem>
-      )}
-    </FileButton>
+    <InvMenuItem as="button" icon={<FaUpload />} {...getRootProps()}>
+      {t('workflows.uploadWorkflow')}
+      <input {...getInputProps()} />
+    </InvMenuItem>
   );
 };
 
